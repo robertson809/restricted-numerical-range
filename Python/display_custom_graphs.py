447bd8b7b025cdcdee8f581f3547f8188657dff2
Michael Robertson <michael.robertson121@gmail.com>
2025-04-01 03:17:38 -0400

    minor refactor

--- Python/display_custom_graphs.py
+++ Python/display_custom_graphs.py
@@ -2,9 +2,11 @@ from display_functions import panel_display, restricted_nr
 import numpy as np
 
 
-example = np.array([[3, -1, -1, -1, 0], [0, 1, -1, 0, 0], [-1, -1, 4, -1, -1],
+example = np.array([[3, -1, -1, 0, -1], [-1, 2, -1, 0, 0], [-1, -1, 4, -1, -1],
                         [0, 0, 0, 0, 0], [0, 0, 0, -1, 1]])
 
-panel_display(example, "Michael")
-restricted_nr(example, "Michael's Restricted Numerical Range", name = "Michael")
+name = "Thomas"
+title = "Thomas's Restricted Numerical Range"
+panel_display(example, name)
+restricted_nr(example, title, name)
 
